44a3f14671d8349cf11731cf1d50b841c0801448
Kévin Gomez <contact@kevingomez.fr>
2015-03-29 12:53:26 +0000

    Revert "Remove doctrine"
    This reverts commit 7cfb7a23e4816a2ff83d866c6a030433b0e38999.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -14,6 +14,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\MonologBundle\MonologBundle(),
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
         );
