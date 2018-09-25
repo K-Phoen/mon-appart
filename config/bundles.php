eee9dfcc222ae04ceb3a04a842996e2ce92752f8
Kévin Gomez <contact@kevingomez.fr>
2018-09-25 17:48:20 +0200

    Install swiftmailer

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,5 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     KPhoen\RulerZBundle\KPhoenRulerZBundle::class => ['all' => true],
+    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
 ];
